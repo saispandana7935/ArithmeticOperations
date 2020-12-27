5acbc348509c731a1f50557d60e6b61b397efa15
saispandana7935 <76431282+saispandana7935@users.noreply.github.com>
2020-12-27 15:58:44 +0530

    Update arithmeticComputation.sh
    merged with local master

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,1 +1,26 @@
 #! /bin/bash -x
+echo "welcome"
+read -p "Enter the input a" a
+read -p "Enter the input b" b
+read -p "Enter the input c" c
+
+result=$((a+b*c))
+result1=$((a*b+c))
+result2=$((c+a/b))
+result3=$((a%b+c))
+
+declare -A Results
+Results["a+b*c"]=$result
+Results["a*b+c"]=$result1
+Results["c+a/b"]=$result2
+Results["a%b+c"]=$result3
+
+echo ${Results[@]}
+	val=0
+
+for value in ${Results[@]}
+do
+	values[val]=$value
+	((val++))
+done
+echo "values from dictionary to array" ${values[@]}
